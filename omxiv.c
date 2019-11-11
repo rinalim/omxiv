8869e7f424fd99a1f3dbaf1911f5bd20cf25d2c0
rinalim <46272802+rinalim@users.noreply.github.com>
2019-11-11 17:51:03 +0900

    Update omxiv.c

--- omxiv.c
+++ omxiv.c
@@ -124,7 +124,6 @@ static int getImageListFromFile(char ***list, const char* path){
 			strncpy(imageList[i], line, lineLen);
 			if(imageList[i][lineLen-1] == '\n')
 				imageList[i][lineLen-1] = '\0';
-			printf("%d, %s\n", lineLen, imageList[i]);
 			imageNum++;
 			i++;
 		}
